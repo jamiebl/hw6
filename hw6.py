5e76339da4e39b3eabcb6053e53a9dd4d9ead7e1
jamiebl <33606347+jamiebl@users.noreply.github.com>
2018-10-17 14:42:14 -0400

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -12,20 +12,7 @@ def sumNums(fileName):
     """This function takes in a filename as input and returns a sum of
 
     the counted integers in the file. 
-    """
-    f = open(fileName, "r")
-    txt = f.read()
-
-    #create an emp list of numbers, in the form of a str
-    list_sum = re.findall(r'\d+', txt)
-
-    #convert to int
-    list_s = [int(val) for val in list_sum]
-
-    #Don't forget to close file, otherwise, u get a weird message
-    f.close()
-    return(sum(list_s))
-
+ 
 
 def countWord(fileName, word):
     pass
